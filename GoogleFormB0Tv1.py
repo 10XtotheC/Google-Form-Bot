f27b5f0d4357c888e04bc009792658772a306734
10XtotheC <76854388+10XtotheC@users.noreply.github.com>
2021-12-04 19:48:16 -0600

    Add files via upload

---
+++ GoogleFormB0Tv1.py
@@ -0,0 +1,12 @@
+import time
+import pyautogui
+a = 10
+while a > 9:
+ pyautogui.click(789,605); pyautogui.typewrite("Kernel Sanders")
+ pyautogui.click(694,772)
+ time.sleep(2)
+ pyautogui.click(734,397)
+
+
+ 
+
